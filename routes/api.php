e0e23a16fdbd3f9f7bb579b6d6c064881ae5d145
HO HUU KHANH <46079705+Huukhanh994@users.noreply.github.com>
2021-04-30 13:44:37 +0700

    Finish author

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,19 @@ Route::middleware('auth:api')->prefix('v1')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    // Route::apiResource('/authors', AuthorsController::class);
+
+
+    // Show all authors
+    Route::get('/authors', [AuthorsController::class, 'index'])->name('authors.index');
+    // Show specific author
+    Route::get('/authors/{author}', [AuthorsController::class, 'show'])->name('authors.show');
+
+    // Store author
+    Route::post('/authors', [AuthorsController::class, 'store'])->name('authors.store');
+    // Update author
+    Route::put('/authors/{author}', [AuthorsController::class, 'update'])->name('authors.update');
+    // Delete author;
+    Route::delete('/authors/{author}', [AuthorsController::class, 'destroy'])->name('authors.delete');
 });
